0b3dae72349cf1eeb6ae764f89af1d0668900a34
Vladislav <vlsterekhov@gmail.com>
2021-10-11 11:55:07 +0300

    Deleted queues

--- server/server.py
+++ server/server.py
@@ -6,8 +6,8 @@ import asyncio
 server_id = 'server ' + os.environ['SERVER_ID']
 
 
-async def read(reader, writer, queue):
-    """Reads messages and create task 'wait_and_write'. Fills the queue."""
+async def handle_echo(reader, writer):
+    """Reads seconds and create task 'wait_and_write' for each inputed second"""
     while True:
         data = await reader.readline()
         if not data:
@@ -16,16 +16,13 @@ async def read(reader, writer, queue):
             break
         seconds = data.decode().split('\n')[0]
         print(f'I get {seconds}')
-        queue.put_nowait(seconds)
-        task = asyncio.create_task(wait_and_write(writer, queue))
+        task = asyncio.create_task(wait_and_write(writer, seconds))
         # print(f'i create {task, type(task)}')
         # print('ALL TASKS =', len(asyncio.all_tasks()))
     
     
-async def wait_and_write(writer, queue):
-    """Gets 'seconds' from queue. Sleeps. After that writes message to client"""
-    seconds = await queue.get()
-    queue.task_done()
+async def wait_and_write(writer, seconds):
+    """Sleeps 'seconds'. After that writes message to client"""
     await asyncio.sleep(int(seconds))
     message = f"{server_id}-{seconds}\n"
     if writer.is_closing():
@@ -34,14 +31,7 @@ async def wait_and_write(writer, queue):
         print(f"Send: {message}")
         writer.write(message.encode())
         
-        
-async def handle_echo(reader, writer):
-    """Creates queue and start task 'read'"""
-    queue = asyncio.Queue()
-    await asyncio.create_task(read(reader, writer, queue))
-    await queue.join()
-
-        
+                
 async def main():
     server = await asyncio.start_server(handle_echo, '0.0.0.0', 6000)
     addr = server.sockets[0].getsockname()
